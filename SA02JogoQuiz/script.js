815a2fb37f691d07d55b4b715d83dc40dbbeaa0a
Renanzxy <r029393829671@gmail.com>
2025-04-01 11:29:43 -0300

    01.04.25

--- SA02JogoQuiz/script.js
+++ SA02JogoQuiz/script.js
@@ -1,7 +1,7 @@
-//script para as perguntas do quiz
+// script para as perguntas do quiz
 
 let perguntas = [];
-let perguntasatual = 0;
+let perguntaAtual = 0;
 
 let perguntaElement = document.getElementById("pergunta");
 let opcoesElement = document.getElementById("opcoes");
@@ -9,13 +9,57 @@ let proximaElement = document.getElementById("proxima");
 let resultadoElement = document.getElementById("resultado");
 
 //carregar as perguntas do Arquivo Json
-fetch("perguntas.json").then(response => response.json())
+fetch("perguntas.json")
+.then(response => response.json())
 .then(data =>{
     perguntas = data;
     carregarPerguntas();
-}).catch(error => console.error("Erro ao carregar perguntas", error));
+}).catch(error => console.error("Erro ao carregar perguntas: ", error));
 
-//criar a função para carregar as Perguntas no html
+//criar a função para carregar as Perguntas no Html
 function carregarPerguntas(){
-    
+    resultadoElement.innerText = "";
+    proximaElement.disabled = true;
+
+    if(perguntaAtual>=perguntas.length){// quando o index for maior ou igual ao nº perguntas
+        perguntaElement.innerText = "Quiz Finalizado!";
+        opcoesElement.innerHTML = "";
+        proximaElement.style.display = "none";
+        return;
+    }
+    //escrever código para mostrar as perguntas
+    let p = perguntas[perguntaAtual];//colegação referente ao índice
+    perguntaElement.innerText = p.pergunta;
+    opcoesElement.innerHTML = "";
+    //preencher as opções
+    p.opcoes.forEach( opcao => {
+        let btnOpcao = document.createElement("button");
+        btnOpcao.innerText = opcao;
+        btnOpcao.classList.add("opcao"); //classe opcao -> style
+        btnOpcao.addEventListener("click",() => verificarResposta(opcao, btnOpcao));
+        opcoesElement.appendChild(btnOpcao);
+    });
+
+// função apra verificar as respostas
+function verificarResposta(opcao,btnOpcao){
+    let respostaCorreta = perguntas[perguntaAtual].resposta;
+
+    if(opcao === respostaCorreta){
+        btnOpcao.classList.add("correta");
+        resultadoElement.innerText = "Correto!!!";
+    }else{
+        btnOpcao.classList.add("errado");
+        resultadoElement.innerText = "Errado!! A resposta Correta é "+respostaCorreta;
+    }
+    //configuração para desabilitat os botões
+    document.querySelectorAll(".opcao").forEach( botao => botao.disabled = true);
+    proximaElement.disabled = false;  
+} //fim da função verificarResposta
+
+// adicionar evento para o botão da próxima pergunta
+
+proximaElement.addEventListener("click", () => {
+    perguntaAtual++;
+    carregarPerguntas();
+});
 }
